a5b7679254b8d219aeccbd374852c0af79da5c16
Dylan25 <dylanwelch55@gmail.com>
2018-10-18 23:31:58 +0000

    initial django project

---
+++ jobTrends/elasticsearchapp/admin.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from .models import BlogPost
+# Register your models here.
+
+#Need to register my BlogPost so it shows up in the admin
+admin.site.register(BlogPost)
