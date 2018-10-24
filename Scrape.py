c28f58bcd2f82b592d435a0a1b9d89f6c21bc92f
Dylan25 <dylanwelch55@gmail.com>
2018-10-24 01:39:15 +0000

    Scrapes the plaintext job description of Number_of_jobs listings from Indeed's CS listings

--- Scrape.py
+++ Scrape.py
@@ -1,8 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin
 
-URL = "https://www.indeed.com/jobs?q=computer+science&l="
+
+Number_of_pages = 2
+count = 0
+URL = "https://www.indeed.com/jobs?q=computer+science&start="
 job_URL = "https://www.indeed.com/viewjob?jk="
 results = requests.get(URL)
 soup = BeautifulSoup(results.text, "html.parser")
@@ -22,33 +24,28 @@ def extract_job_links(soup):
     return done_links
 
 
-def extract_job_summary(soup):
-    summaries = []
-    spans = soup.findAll('span', attrs={'class' : 'summary'})
-    for span in spans:
-        summaries.append(span.text.strip())
-    return summaries
-
-
-def extract_job_titles(soup):
-    jobTitles = []
-    for div in soup.findAll(name='div', attrs={"class" : "row"}):
-        for a in div.find_all(name="a", attrs={"data-tn-element" : "jobTitle"}):
-            jobTitles.append(a["title"])
-    return jobTitles
+def get_job_description(URL):
+    job = requests.get(URL)
+    if job.status_code != 200:
+        print("Error: " + job.status_code)
+    else:
+        soup = BeautifulSoup(job.text, "html.parser")
+        result = soup.find(name='div', attrs={"class": "jobsearch-JobComponent-description icl-u-xs-mt--md"})
+        return result.get_text()
 
 
 if results.status_code != 200:
     print("Error: " + results.status_code)
 else:
-    for i, j in zip(extract_job_titles(result), extract_job_summary(result)):
+    for i in range(Number_of_pages):
+        URL = URL + str(count * 10)
+        results = requests.get(URL)
+        soup = BeautifulSoup(results.text, "html.parser")
+        result = soup.find(id='resultsCol')
+        for k in extract_job_links(result):
+            print("\n")
+            print(k)
+            print("\n")
+            print(get_job_description(k))
         print("\n")
-        print("NEW JOB************")
-        print(i, j)
-        print("\n")
-
-    print("links: ")
-    for k in extract_job_links(result):
-        print("\n")
-        print(k)
-    print("\n")
+    print("done:\n")
