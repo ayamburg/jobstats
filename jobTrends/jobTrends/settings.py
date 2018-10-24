b1b0700b3bdbad1790069f88fb406563b05cea8b
Dylan25 <dylanwelch55@gmail.com>
2018-10-24 22:50:42 +0000

    added landing page functionality

--- jobTrends/jobTrends/settings.py
+++ jobTrends/jobTrends/settings.py
@@ -55,7 +55,8 @@ ROOT_URLCONF = 'jobTrends.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'jobTrends', 'templates', 'jobTrends')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,5 +118,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+STATICFILES_DIRS = (
+     os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 
 STATIC_URL = '/static/'
