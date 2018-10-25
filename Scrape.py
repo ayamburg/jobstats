324055062c8f4ea33f6e90cc6159e8940707c74a
Dylan25 <dylanwelch55@gmail.com>
2018-10-25 23:54:20 +0000

    everything works, just need to put it into db model

--- Scrape.py
+++ Scrape.py
@@ -37,46 +37,37 @@ def extract_location_from_result(soup):
 
 def get_company(soup):
     companies = []
-    for div in soup.findAll(name='div', attrs={'class': 'row'}):
-        name = div.findAll(name='span', attrs={'class': 'company'})
+    for div in soup.find_all(name='div', attrs={'class': 'row'}):
+        name = div.find_all(name='span', attrs={'class': 'company'})
         if len(name) > 0:
             for x in name:
                 companies.append(x.text.strip())
         else:
             other = div.find_all(name='span', attrs={'class': 'result-linklsource'})
-            for span in other:
-                companies.append(span.text.strip())
-        return companies
+            for y in other:
+                companies.append(y.text.strip())
+    return companies
 
 
-def get_everything(one_job_URL, location, company_name):
-    indeed_id = one_job_URL
-    listing_URL = job_URL + one_job_URL
-    job = requests.get(listing_URL)
-    print(listing_URL)
-    if job.status_code != 200:
-        print("Error: " + job.status_code)
-    else:
-        soup = BeautifulSoup(job.text, "html.parser")
-        result = soup.find(name='div', attrs={"class": "jobsearch-JobComponent-description icl-u-xs-mt--md"})
-        title = soup.find(name='h3', attrs={"class" : "icl-u-xs-mb--xs icl-u-xs-mt--none jobsearch-JobInfoHeader-title"})
-        # get the job posted time and clean it to "1 day ago" format
-        date_info = soup.find(name='div', attrs={"class" : "jobsearch-JobMetadataFooter"})
-        date_text = date_info.get_text()
-        num = re.search(r'\d+',  date_text).group()
-        #print(date_text)
-        date_posted = get_date_posted(date_text, currentDT, num)
-        #location = soup.find(name='span', attrs={'class': 'location'}).get_text()
-        print(company_name)
-        print(location)
-        print(indeed_id)
-        print(date_posted)
-        print(title.get_text())
-        print("\n")
-        return result.get_text()
+def get_description(soup):
+    result = soup.find(name='div', attrs={"class": "jobsearch-JobComponent-description icl-u-xs-mt--md"})
+    return result.get_text()
 
 
-def get_date_posted(relative_time, currentDT, num):
+def get_title(soup):
+    title = soup.find(name='h3', attrs={"class": "icl-u-xs-mb--xs icl-u-xs-mt--none jobsearch-JobInfoHeader-title"})
+    return title.get_text()
+
+
+def get_date_posted(soup):
+    date_info = soup.find(name='div', attrs={"class": "jobsearch-JobMetadataFooter"})
+    date_text = date_info.get_text()
+    num = re.search(r'\d+', date_text).group()
+    date_posted = process_date_posted(date_text, currentDT, num)
+    return date_posted
+
+
+def process_date_posted(relative_time, currentDT, num):
     if relative_time.find('+') != -1:
         difference = currentDT - datetime.timedelta(days=31)
         return difference.date()
@@ -102,9 +93,24 @@ else:
         results = requests.get(URL)
         soup = BeautifulSoup(results.text, "html.parser")
         result = soup.find(id='resultsCol')
-        names = get_company(result)
+        locations = extract_location_from_result(result)
+        names = get_company(soup)
         counter = 0
-        for j, k in zip(extract_job_links(result), extract_location_from_result(result)):
-            get_everything(j, k, names[counter])
-            counter + 1
+        for j in extract_job_links(result):
+            listing_URL = job_URL + j
+            single_job = requests.get(listing_URL)
+            if single_job.status_code != 200:
+                print("Error: " + single_job.status_code)
+            else:
+                job_soup = BeautifulSoup(single_job.text, "html.parser")
+                print("LISTING...")
+                print(counter)
+                print(j)  # jk indeed id
+                get_description(job_soup)
+                print(get_title(job_soup))
+                print(get_date_posted(job_soup))
+                print(locations[counter])
+                print(names[counter])
+                print("\n")
+            counter = counter + 1
     print("done:\n")
