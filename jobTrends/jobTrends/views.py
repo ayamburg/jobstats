57906d57b0a289e6fb94fb083e12ff2a7d83e83f
ayamburg <ayamburg@ucsc.edu>
2018-11-04 21:42:02 -0800

    Add filtering using elastic search

--- jobTrends/jobTrends/views.py
+++ jobTrends/jobTrends/views.py
@@ -3,14 +3,26 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.apps import apps
+from elasticsearchapp.documents import JobListingDocument
+from elasticsearch_dsl import Q
 JobListing = apps.get_model('elasticsearchapp', 'JobListing')
 
 
 # Create your views here.
 def home(request):
-    data = JobListing.objects.all()
+    params = request.GET.getlist('keywords[]')
+    queries = Q()
+
+    for param in params:
+        queries = queries & Q("match", description=param)
+
+    listings_search = JobListingDocument.search().query(queries)
+
+    total = listings_search.count()
+    listings_search = listings_search[0:total]
+
     listings = {
-        "job_listings": data
+        'job_listings': listings_search
     }
 
     return render(request, 'JobTrendsLandingPage.html', listings)
