53e18199430126a594dd0a81a8e175681a6847ee
ayamburg <ayamburg@ucsc.edu>
2018-11-06 19:14:06 -0800

    Connect graph to landing page and db

--- jobTrends/jobTrends/urls.py
+++ jobTrends/jobTrends/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home),
+    url(r'job-trends.html/', TemplateView.as_view(template_name="job-trends.html"), name='job-trends'),
 ]
 
 
--- jobTrends/jobTrends/views.py
+++ jobTrends/jobTrends/views.py
@@ -1,28 +1,51 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from datetime import datetime
 from django.shortcuts import render
 from django.apps import apps
 from elasticsearchapp.documents import JobListingDocument
 from elasticsearch_dsl import Q
+import numpy as np
+import plotly.graph_objs as go
+import plotly.offline as ply
+import plotly.tools as tls
 JobListing = apps.get_model('elasticsearchapp', 'JobListing')
 
 
-# Create your views here.
 def home(request):
-    params = request.GET.getlist('keywords[]')
+    filters = request.GET.getlist('filters[]')
+    keywords = request.GET.getlist('keywords[]')
     queries = Q()
 
-    for param in params:
-        queries = queries & Q("match", description=param)
+    for f in filters:
+        queries = queries & Q("match", description=f)
 
-    listings_search = JobListingDocument.search().query(queries)
+    data = []
 
-    total = listings_search.count()
-    listings_search = listings_search[0:total]
+    for keyword in keywords:
+        query = queries & Q("match", description=keyword)
+        listings_search = JobListingDocument.search().query(query)
+        total = listings_search.count()
+        listings_search = listings_search[0:total]
 
-    listings = {
-        'job_listings': listings_search
-    }
+        listings_search.aggs.bucket('listings_per_day', 'date_histogram', field='posted_date', interval='day')
+        listings_search = listings_search.execute()
 
-    return render(request, 'JobTrendsLandingPage.html', listings)
+        x = []
+        y = []
+        for bucket in listings_search.aggregations.listings_per_day.buckets:
+            print(bucket.key)
+            print(bucket.key_as_string)
+            x.append(datetime.utcfromtimestamp(bucket.key/1000).strftime('%x'))
+            y.append(bucket.doc_count)
+
+        trace = go.Scatter(
+            x=x,
+            y=y,
+            name=keyword,
+            mode='lines+markers'
+        )
+        data.append(trace)
+    ply.plot(data, filename='templates/job-trends.html', auto_open=False)
+
+    return render(request, 'JobTrendsLandingPage.html')
