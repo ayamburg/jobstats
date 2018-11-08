1508e43a4629bb15a2a5f6bfd4a4879348291bd5
ayamburg <ayamburg@ucsc.edu>
2018-11-07 21:35:58 -0800

    Add percents

--- jobTrends/jobTrends/views.py
+++ jobTrends/jobTrends/views.py
@@ -15,6 +15,7 @@ JobListing = apps.get_model('elasticsearchapp', 'JobListing')
 def home(request):
     filters = request.GET.getlist('filters[]')
     keywords = request.GET.getlist('keywords[]')
+    raw = request.GET.get('raw')
     queries = Q()
     if not keywords:
         keywords = ['Java', 'Python', 'Ruby', 'PHP', 'iOS', 'Android']
@@ -24,21 +25,36 @@ def home(request):
 
     data = []
 
+    # calculate total for each day
+    total_search = JobListingDocument.search()
+    total = total_search.count()
+    total_search = total_search[0:total]
+    total_search.aggs.bucket('listings_per_day', 'date_histogram', field='posted_date', interval='day')
+    total_search = total_search.execute()
+    total_buckets = total_search.aggregations.listings_per_day.buckets
+
+    # calculate number of entries and add trace for each keyword
     for keyword in keywords:
         query = queries & Q("match", description=keyword)
         listings_search = JobListingDocument.search().query(query)
-        total = listings_search.count()
-        listings_search = listings_search[0:total]
+        key_total = listings_search.count()
+        listings_search = listings_search[0:key_total]
 
         listings_search.aggs.bucket('listings_per_day', 'date_histogram', field='posted_date', interval='day')
         listings_search = listings_search.execute()
 
         x = []
         y = []
-        for bucket in listings_search.aggregations.listings_per_day.buckets:
+        for index, bucket in enumerate(listings_search.aggregations.listings_per_day.buckets):
             if bucket.key >= 1541203200000:
                 x.append(datetime.utcfromtimestamp(bucket.key/1000).strftime('%x'))
-                y.append(bucket.doc_count)
+                if raw == '0':
+                    try:
+                        y.append(bucket.doc_count / total_buckets[index].doc_count)
+                    except ZeroDivisionError:
+                        y.append(0)
+                else:
+                    y.append(bucket.doc_count)
 
         trace = go.Scatter(
             x=x,
@@ -48,7 +64,11 @@ def home(request):
         )
         data.append(trace)
 
-    layout = go.Layout(showlegend=True)
+    if raw == '0':
+        layout = go.Layout(showlegend=True, yaxis=dict(tickformat=".2%"))
+
+    else:
+        layout = go.Layout(showlegend=True)
     fig = go.Figure(data=data, layout=layout)
     ply.plot(fig, filename='templates/job-trends.html', auto_open=False)
 
