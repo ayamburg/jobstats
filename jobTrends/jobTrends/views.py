d9b109e6d8ac490b8393fa637e17804c38252428
ayamburg <ayamburg@ucsc.edu>
2018-11-15 16:16:43 -0800

    Add percent value display

--- jobTrends/jobTrends/views.py
+++ jobTrends/jobTrends/views.py
@@ -11,12 +11,13 @@ import plotly.offline as ply
 import plotly.tools as tls
 JobListing = apps.get_model('elasticsearchapp', 'JobListing')
 
+SCRAPE_DATA_START = 1541203200000
+
 
 def home(request):
     filters = request.GET.getlist('filters[]')
     keywords = request.GET.getlist('keywords[]')
-    # raw = request.GET.get('raw')
-    raw = 1
+    raw = request.GET.get('raw')
     queries = Q()
     if not keywords:
         keywords = ['Java', 'Python', 'Ruby', 'PHP', 'iOS', 'Android']
@@ -26,14 +27,8 @@ def home(request):
 
     data = []
 
-    if raw == '0':
-        # calculate total for each day
-        total_search = JobListingDocument.search()
-        total = total_search.count()
-        total_search = total_search[0:total]
-        total_search.aggs.bucket('listings_per_day', 'date_histogram', field='posted_date', interval='day')
-        total_search = total_search.execute()
-        total_buckets = total_search.aggregations.listings_per_day.buckets
+    if raw != '1':
+        total_y = calculate_totals()
 
     # calculate number of entries and add trace for each keyword
     for keyword in keywords:
@@ -48,11 +43,11 @@ def home(request):
         x = []
         y = []
         for index, bucket in enumerate(listings_search.aggregations.listings_per_day.buckets):
-            if bucket.key >= 1541203200000:
+            if bucket.key >= SCRAPE_DATA_START:
                 x.append(datetime.utcfromtimestamp(bucket.key/1000).strftime('%x'))
-                if raw == '0':
+                if raw != '1':
                     try:
-                        y.append(bucket.doc_count / total_buckets[index].doc_count)
+                        y.append(bucket.doc_count / total_y[index])
                     except ZeroDivisionError:
                         y.append(0)
                 else:
@@ -66,12 +61,25 @@ def home(request):
         )
         data.append(trace)
 
-    if raw == '0':
+    if raw != '1':
         layout = go.Layout(showlegend=True, yaxis=dict(tickformat=".2%"))
-
     else:
         layout = go.Layout(showlegend=True)
     fig = go.Figure(data=data, layout=layout)
     ply.plot(fig, filename='templates/job-trends.html', auto_open=False)
 
     return render(request, 'JobTrendsLandingPage.html')
+
+
+def calculate_totals():
+    total_search = JobListingDocument.search()
+    total = total_search.count()
+    total_search = total_search[0:total]
+    total_search.aggs.bucket('listings_per_day', 'date_histogram', field='posted_date', interval='day')
+    total_search = total_search.execute()
+    total_buckets = total_search.aggregations.listings_per_day.buckets
+    total_y = []
+    for bucket in total_buckets:
+        if bucket.key >= SCRAPE_DATA_START:
+            total_y.append(bucket.doc_count)
+    return total_y
