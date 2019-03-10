944478fbaae80ba5672d69c1bd88d91485757a86
ayamburg <ayamburg@ucsc.edu>
2019-03-09 21:54:47 -0800

    Merge up and down insights

--- jobTrends/elasticsearchapp/management/commands/insights.py
+++ jobTrends/elasticsearchapp/management/commands/insights.py
@@ -1,28 +1,7 @@
-from data import pct_fran
-import json
 import pandas as pd
 import numpy as np
-from tabulate import tabulate
-import matplotlib.pyplot as plt
-
-# keywords = data['keywords']
-# input_data = dict(zip(keywords, data['y']))
 
 #scan folder containing dataset json objects and load them in
-json_objects=[]
-with open('pct_fran.json', 'r') as data_file:
-    json_objects.append(json.load(data_file))
-with open('fran.json', 'r') as data_file:
-    json_objects.append(json.load(data_file))
-with open('pct_bost.json', 'r') as data_file:
-    json_objects.append(json.load(data_file))
-with open('bost.json', 'r') as data_file:
-    json_objects.append(json.load(data_file))
-with open('pct.json','r') as data_file:
-    json_objects.append(json.load(data_file))
-with open('data.json','r') as data_file:
-    json_objects.append(json.load(data_file))
-
 def create_df(dataset):
     #create dictionary out of keywords and associated data, creating a Series out of each data list
     #so that we can create a dataframe with all keywords in it, even if some keywords have fewer
@@ -57,7 +36,7 @@ def get_trending_up(dataset):
     """Returns the skill with highest upward trend, its slope, and confidence score.
 
     Arguments: Takes in 1 dict
-    Constraints: The data should be in percent format.
+    Constraints: The data should be daily and in percent format.
     """
     insight_hash = {}
     insight_hash['type'] = 'Trending Up'
@@ -77,10 +56,10 @@ def get_trending_down(dataset):
     """Returns the skill with lowest downward trend, its slope, and confidence score.
 
     Arguments: Takes in a single dataset in the form of a dict
-    Constraints: The data should be in percent format.
+    Constraints: The data should be daily and in percent format.
     """
     insight_hash = {}
-    insight_hash['Type'] = 'Trending Down'
+    insight_hash['type'] = 'Trending Down'
     df = create_df(dataset)
     kw = {}
     maxVal=0
@@ -88,8 +67,8 @@ def get_trending_down(dataset):
     for col in df:
         slopes[trendline(df[[col]])[0]] = col
     sort = sorted(slopes)
-    insight_hash['Skill'] = slopes[sort[0]]
-    insight_hash['Value'] = sort[0]
+    insight_hash['skill'] = slopes[sort[0]]
+    insight_hash['value'] = sort[0]
     return insight_hash
 
 def get_skill_location(city_datasets,non_city_dataset):
@@ -170,36 +149,8 @@ def get_correlation(dataset):
     df = create_df(dataset).corr().replace(to_replace=1,value=0)
     max=df.values.max()
     print(df.max().max(), "\n", df.max().max().index)
-    # for i in range(len(df)):
 
 
-
-    # for i in (range(len(self.keywords)-1)):
-    #     for j in range(i,len(self.keywords)):
-    #         corr = self.dataset[self.keywords[i]].corr(self.dataset[self.keywords[j]], method='spearman')
-    #         if corr > .90 and (self.keywords[i] != self.keywords[j]):
-    #             print("significant correlation between %s and %s" % (self.keywords[i],self.keywords[j]))
-
-city_data = {}
-main_data = {}
-
-for dataset in json_objects:
-    if 'location' in dataset: # may remove if all jsons will have this key, ask andrey
-        if dataset['location']:
-            if dataset['location'] not in city_data:
-                city_data[dataset['location']] = {}
-            if dataset['raw'] == '1':
-                city_data[dataset['location']][1] = (create_df(dataset))
-            else:
-                city_data[dataset['location']][0] = (create_df(dataset))
-    else:
-        if dataset['raw'] == '1':
-            main_data[1] = create_df(dataset)
-        else:
-            main_data[0] = create_df(dataset)
-
-#if at some point we can figure out how to ensure that all data sets have
-#equal length, then the following algorithm is simpler and faster
 
 # trendline(city_data['boston'][0][['python']])
 # print(city_data['boston'][0][['javascript']].rolling(7,center=True).mean().dropna(axis=0).resample('M').mean())
@@ -208,5 +159,3 @@ for dataset in json_objects:
 # print(get_dominant_skill(json_objects[2]))
 # get_skill_location([json_objects[1],json_objects[3]], json_objects[5])
 # get_correlation(json_objects[4])
-
-print("end of program")
