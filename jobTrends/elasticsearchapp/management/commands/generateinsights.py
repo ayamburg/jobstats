e3edb2c2e6a1874b9e8f8a2a03b3f4028599001a
ayamburg <ayamburg@ucsc.edu>
2019-03-10 16:36:18 -0700

    Make top skills and insight blocks into functions

--- jobTrends/elasticsearchapp/management/commands/generateinsights.py
+++ jobTrends/elasticsearchapp/management/commands/generateinsights.py
@@ -20,23 +20,51 @@ cities = ['new york',
           'san francisco']
 
 
-def frontend():
+def generate_insights_for_location_page(name, filters, companies, titles, locations):
     # read top skills
-    file = open(top_skills_path + "frontend.json")
+    file = open(top_skills_path + name + ".json")
     skills_hash = json.load(file)['skills']
     top_skills = []
     for skill in skills_hash:
         top_skills.append(skill['key'])
 
     # get data
-    percent_trend_data = data_handler.get_trend_data([], top_skills, '0', 'day', [], ['front end', 'frontend'], [])
-    raw_trend_data = data_handler.get_trend_data([], top_skills, '1', 'day', [], ['front end', 'frontend'], [])
+    percent_trend_data = data_handler.get_trend_data(filters, top_skills, '0', 'day', companies, titles, locations)
+    weekly_trend_data = data_handler.get_trend_data(filters, top_skills, '0', 'week', companies, titles, locations)
+    daily_trend_data = data_handler.get_trend_data(filters, top_skills, '0', 'day', companies, titles, locations)
+
+    # get insights
+    insights = []
+    insights.append(get_trending_up(percent_trend_data))
+    insights.append(get_trending_down(percent_trend_data))
+    insights.append(get_dominant_skill(weekly_trend_data))
+    insights.append(get_correlation(daily_trend_data))
+
+    # write to file
+    timestamp = str(datetime.datetime.now())
+    if os.path.isfile(insights_path + name + ".json"):
+        os.rename(insights_path + name + ".json", insights_path + "historical/{0}{1}.json".format(name, timestamp))
+    write_file = open(insights_path + name + ".json", "w+")
+    json.dump({'insights': insights}, write_file)
+
+
+def generate_insights(name, filters, companies, titles):
+    # read top skills
+    file = open(top_skills_path + name + ".json")
+    skills_hash = json.load(file)['skills']
+    top_skills = []
+    for skill in skills_hash:
+        top_skills.append(skill['key'])
+
+    # get data
+    percent_trend_data = data_handler.get_trend_data(filters, top_skills, '0', 'day', companies, titles, [])
+    raw_trend_data = data_handler.get_trend_data(filters, top_skills, '1', 'day', companies, titles, [])
     city_datasets = []
     for city in cities:
         city_datasets.append(
-            data_handler.get_trend_data([], top_skills, '1', 'day', [], ['front end', 'frontend'], [city]))
-    weekly_trend_data = data_handler.get_trend_data([], top_skills, '0', 'week', [], ['front end', 'frontend'], [])
-    daily_trend_data = data_handler.get_trend_data([], top_skills, '0', 'day', [], ['front end', 'frontend'], [])
+            data_handler.get_trend_data(filters, top_skills, '1', 'day', companies, titles, [city]))
+    weekly_trend_data = data_handler.get_trend_data(filters, top_skills, '0', 'week', companies, titles, [])
+    daily_trend_data = data_handler.get_trend_data(filters, top_skills, '0', 'day', companies, titles, [])
 
     # get insights
     insights = []
@@ -48,9 +76,9 @@ def frontend():
 
     # write to file
     timestamp = str(datetime.datetime.now())
-    if os.path.isfile(insights_path + "frontend.json"):
-        os.rename(insights_path + "frontend.json", insights_path + "historical/frontend{0}.json".format(timestamp))
-    write_file = open(insights_path + "frontend.json", "w+")
+    if os.path.isfile(insights_path + name + ".json"):
+        os.rename(insights_path + name + ".json", insights_path + "historical/{0}{1}.json".format(name, timestamp))
+    write_file = open(insights_path + name + ".json", "w+")
     json.dump({'insights': insights}, write_file)
 
 
@@ -58,4 +86,4 @@ class Command(BaseCommand):
     help = 'Generates Insights'
 
     def handle(self, *args, **options):
-        frontend()
+        generate_insights('frontend', [], [], ['front end', 'frontend'])
