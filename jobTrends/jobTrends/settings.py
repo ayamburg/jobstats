6ccaef027b3d4a2632c74031dd78042f1f5eba5b
ayamburg <ayamburg@ucsc.edu>
2019-04-15 03:14:04 -0700

    Fix oauth linkedin

--- jobTrends/jobTrends/settings.py
+++ jobTrends/jobTrends/settings.py
@@ -60,7 +60,7 @@ SOCIALACCOUNT_QUERY_EMAIL = True
 SOCIALACCOUNT_PROVIDERS = {
     'linkedin_oauth2': {
         'SCOPE': [
-            'r_basicprofile',
+            'r_liteprofile',
             'r_emailaddress'
         ],
         'PROFILE_FIELDS': [
