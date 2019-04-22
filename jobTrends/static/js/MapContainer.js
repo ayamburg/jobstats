be662aed75d88d2d53b379733fe029cc4ef176b5
Dylan <dylanwelch55@gmail.com>
2019-04-21 20:07:47 -0700

    added side bar

--- jobTrends/static/js/MapContainer.js
+++ jobTrends/static/js/MapContainer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { GoogleApiWrapper, InfoWindow, Map, Marker } from 'google-maps-react';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import ClippedDrawer from './MapSideBar.js';
 
 
 class GoogleMapsContainer extends React.Component {
@@ -33,47 +34,52 @@ class GoogleMapsContainer extends React.Component {
   }
   render() {
     const style = {
-      width: '50vw',
-      height: '75vh',
+      //width: '50vw',
+      //height: '75vh',
       'marginLeft': 'auto',
       'marginRight': 'auto'
     }
     return (
-      <Map
-        item
-        xs = { 12 }
-        style = { style }
-        google = { this.props.google }
-        onClick = { this.onMapClick }
-        zoom = { 14 }
-        initialCenter = {{ lat: 39.648209, lng: -75.711185 }}
-      >
-        <Marker
-          onClick = { this.onMarkerClick }
-          title = { 'Changing Colors Garage' }
-          position = {{ lat: 39.648209, lng: -75.711185 }}
-          name = { 'Changing Colors Garage' }
-        />
-        <InfoWindow
-          marker = { this.state.activeMarker }
-          visible = { this.state.showingInfoWindow }
-        >
-          <Paper>
-            <Typography
-              variant = 'headline'
-              component = 'h4'
+        <div>
+            <Map
+                item
+                xs = { 12 }
+                style = { style }
+                google = { this.props.google }
+                onClick = { this.onMapClick }
+                zoom = { 14 }
+                initialCenter = {{ lat: 39.648209, lng: -75.711185 }}
             >
-              Changing Colors Garage
-            </Typography>
-            <Typography
-              component = 'p'
-            >
-              98G Albe Dr Newark, DE 19702 <br />
-              302-293-8627
-            </Typography>
-          </Paper>
-        </InfoWindow>
-      </Map>
+                <ClippedDrawer>
+                    
+                </ClippedDrawer>
+                <Marker
+                onClick = { this.onMarkerClick }
+                title = { 'Changing Colors Garage' }
+                position = {{ lat: 39.648209, lng: -75.711185 }}
+                name = { 'Changing Colors Garage' }
+                />
+                <InfoWindow
+                marker = { this.state.activeMarker }
+                visible = { this.state.showingInfoWindow }
+                >
+                <Paper>
+                    <Typography
+                    variant = 'headline'
+                    component = 'h4'
+                    >
+                    Changing Colors Garage
+                    </Typography>
+                    <Typography
+                    component = 'p'
+                    >
+                    98G Albe Dr Newark, DE 19702 <br />
+                    302-293-8627
+                    </Typography>
+                </Paper>
+                </InfoWindow>
+            </Map>
+        </div>
     );
   }
 }
