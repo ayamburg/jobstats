99926ee5579e25caf2367f80eeb7fef9254a7153
ayamburg <ayamburg@ucsc.edu>
2019-04-24 19:16:38 -0700

    Add tile model

--- jobTrends/elasticsearchapp/admin.py
+++ jobTrends/elasticsearchapp/admin.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from .models import JobListing
+from .models import Tile
 
 # Register your models here.
 admin.site.register(JobListing)
+admin.site.register(Tile)
