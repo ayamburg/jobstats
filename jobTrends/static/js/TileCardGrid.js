dd38b67ce479e3b47a0efa629993439f8b81f31f
ayamburg <ayamburg@ucsc.edu>
2019-05-02 02:32:35 -0700

    Add custom tiles on UI

--- jobTrends/static/js/TileCardGrid.js
+++ jobTrends/static/js/TileCardGrid.js
@@ -4,68 +4,109 @@ import React from 'react'
 import Grid from '@material-ui/core/Grid';
 import TileCard from './TileCard.js'
 import Icon from '@material-ui/core/Icon';
-import { Typography } from '@material-ui/core';
+import {Typography} from '@material-ui/core';
+import axios from "axios";
 
 class TileCardGrid extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
+        this.state = {
+            custom_tiles: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get('custom_tiles', {
+            responseType: 'json'
+        }).then(response => {
+            this.setState({custom_tiles: response.data.custom_tiles});
+        });
+    }
+
+    loadCustomTiles() {
+        let custom_tiles = this.state.custom_tiles;
+        let custom_tile_cards = [];
+
+        for (let i = 0; i < custom_tiles.length; i++) {
+            let title = custom_tiles[i].title;
+            let path = "/" + custom_tiles[i].id;
+            custom_tile_cards.push(
+                <Grid item xs={"auto"} key={i}>
+                    <TileCard path={path} cardTitle={title}
+                              backColor={{background: 'linear-gradient(45deg, #ffffff 17%, #ffffff 90%)'}}>
+
+                    </TileCard>
+                </Grid>
+            )
+        }
+        return custom_tile_cards;
     }
 
     render() {
-
-        return(
+        return (
             <Grid
                 style={{
-                width: '100%',
+                    width: '100%',
                 }}
-                container 
+                container
                 spacing={16}
-                alignContent = "flex-end"
-                alignItems = "flex-end"
-                justify = "center"
+                alignContent="flex-end"
+                alignItems="flex-end"
+                justify="center"
             >
                 <Grid item xs={"auto"}>
-                    <TileCard image = "/static/images/amazonlogo.png" path="/amazon" cardTitle="Top Skills for Amazon" backColor={{background: 'linear-gradient(45deg, #ffffff 17%, ##ffffff 100%)'}}/>
+                    <TileCard image="/static/images/amazonlogo.png" path="/amazon" cardTitle="Top Skills for Amazon"
+                              backColor={{background: 'linear-gradient(45deg, #ffffff 17%, ##ffffff 100%)'}}/>
                 </Grid>
                 <Grid item xs={"auto"}>
-                    <TileCard image="/static/images/applelogo.svg" path="/apple" cardTitle="Top Skills for Apple" backColor={{background: 'linear-gradient(to top right, #ffffff -21%, #ffffff 100%)'}}>
-                        
+                    <TileCard image="/static/images/applelogo.svg" path="/apple" cardTitle="Top Skills for Apple"
+                              backColor={{background: 'linear-gradient(to top right, #ffffff -21%, #ffffff 100%)'}}>
+
                     </TileCard>
                 </Grid>
                 <Grid item xs={"auto"}>
-                    <TileCard image="/static/images/googlelogo.svg" path="/google" cardTitle="Top Skills for Google" backColor={{background: 'linear-gradient(45deg, #ffffff 17%, #ffffff 90%)'}}>
-                            
+                    <TileCard image="/static/images/googlelogo.svg" path="/google" cardTitle="Top Skills for Google"
+                              backColor={{background: 'linear-gradient(45deg, #ffffff 17%, #ffffff 90%)'}}>
+
                     </TileCard>
                 </Grid>
                 <Grid item xs={"auto"}>
-                    <TileCard image="/static/images/microsoftlogo.svg" path="/microsoft" cardTitle="Top Skills for Microsoft" backColor={{background: 'linear-gradient(45deg, #ffffff 17%, #ffffff 100%)'}}>
-                        
+                    <TileCard image="/static/images/microsoftlogo.svg" path="/microsoft"
+                              cardTitle="Top Skills for Microsoft"
+                              backColor={{background: 'linear-gradient(45deg, #ffffff 17%, #ffffff 100%)'}}>
+
                     </TileCard>
                 </Grid>
                 <Grid item xs={"auto"}>
-                    <TileCard path="/frontend" cardTitle="Top Front End Skills" backColor={{background: 'linear-gradient(45deg, #ffffff 17%, #ffffff 90%)'}}>
-                        
-                    </TileCard>  
+                    <TileCard path="/frontend" cardTitle="Top Front End Skills"
+                              backColor={{background: 'linear-gradient(45deg, #ffffff 17%, #ffffff 90%)'}}>
+
+                    </TileCard>
                 </Grid>
                 <Grid item xs={"auto"}>
-                    <TileCard path="/backend" cardTitle="Top Back End Skills" backColor={{background: 'linear-gradient(45deg, #ffffff 17%, #ffffff 90%)'}}>
-                        
-                    </TileCard>  
+                    <TileCard path="/backend" cardTitle="Top Back End Skills"
+                              backColor={{background: 'linear-gradient(45deg, #ffffff 17%, #ffffff 90%)'}}>
+
+                    </TileCard>
                 </Grid>
                 <Grid item xs={"auto"}>
-                    <TileCard path="/fullstack" cardTitle="Top Full Stack Skills" backColor={{background: 'linear-gradient(45deg, #ffffff 17%, #ffffff 90%)'}}>
-                        
-                    </TileCard>  
+                    <TileCard path="/fullstack" cardTitle="Top Full Stack Skills"
+                              backColor={{background: 'linear-gradient(45deg, #ffffff 17%, #ffffff 90%)'}}>
+
+                    </TileCard>
                 </Grid>
                 <Grid item xs={"auto"}>
-                    <TileCard  path="/cybersecurity" cardTitle="Top Cyber Security Skills" backColor={{background: 'linear-gradient(45deg, #ffffff 17%, #ffffff 90%)'}}>
-                        
-                    </TileCard>  
+                    <TileCard path="/cybersecurity" cardTitle="Top Cyber Security Skills"
+                              backColor={{background: 'linear-gradient(45deg, #ffffff 17%, #ffffff 90%)'}}>
+
+                    </TileCard>
                 </Grid>
+                {this.loadCustomTiles()}
                 <Grid item xs={"auto"}>
-                    <TileCard image = "/static/images/baseline-add-24px.svg" path="/custom" cardTitle="Add Custom Tile" backColor={{background: 'linear-gradient(45deg, #ffffff 17%, #ffffff 90%)'}}>
-                    
-                    </TileCard>  
+                    <TileCard image="/static/images/baseline-add-24px.svg" path="/custom" cardTitle="Add Custom Tile"
+                              backColor={{background: 'linear-gradient(45deg, #ffffff 17%, #ffffff 90%)'}}>
+
+                    </TileCard>
                 </Grid>
             </Grid>
         );
