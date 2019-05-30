f5737ea402f951dbc628243723fcddc4f4a70d2f
ayamburg <ayamburg@ucsc.edu>
2019-05-29 17:24:29 -0700

    Remove state from city name

--- jobTrends/elasticsearchapp/management/commands/generateinsights.py
+++ jobTrends/elasticsearchapp/management/commands/generateinsights.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from elasticsearchapp.tile_models import Tile
+import time
 
 
 class Command(BaseCommand):
@@ -8,4 +9,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         tiles = Tile.objects.all()
         for tile in tiles:
+            start_time = time.time()
+            print("<<< Generating insights for: %s >>>" % tile.name)
             tile.generate_insights()
+            print("<<< %s: %s seconds >>>" % (tile.name, time.time() - start_time))
--- jobTrends/elasticsearchapp/migrations/0022_make_location_tiles.py
+++ jobTrends/elasticsearchapp/migrations/0022_make_location_tiles.py
@@ -5,29 +5,17 @@ import re
 
 
 def create_location_tiles(apps, schema_editor):
-
-    # new_custom_tile = StandardTile.objects.create(
-    #     filters=[],
-    #     locations=['pizzatown'],
-    #     companies=[],
-    #     titles=[],
-    #     blacklists=[],
-    #     whitelists=[],
-    #     title='pizzatown',
-    #     name='pizzatown')
-    # new_custom_tile.save()e
-
     regex = re.compile('[^a-zA-Z]')
-    #First parameter is the replacement, second parameter is your input string   
+    # First parameter is the replacement, second parameter is your input string
 
     include = open("jobTrends/location_data/col.json", "r")
     include = json.load(include)
     for city in include.keys():
-        print("city is: ", city)
+        print("city is: ", city.split(',')[0])
         print("name is: ", regex.sub('', city))
         new_custom_tile = StandardTile.objects.create(
             filters=[],
-            locations=[city],
+            locations=[city.split(',')[0]],
             companies=[],
             titles=[],
             blacklists=[],
@@ -35,6 +23,7 @@ def create_location_tiles(apps, schema_editor):
             title=city,
             name=regex.sub('', city))
         new_custom_tile.save()         
+
 
 class Migration(migrations.Migration):
     dependencies = [
